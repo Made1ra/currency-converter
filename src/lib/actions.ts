52ba44c265b62b62ed623340e7dcfa022d776800
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-02-24 23:37:12 +0200

    Changed formatting

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `https://api.exchangerate.host`;
+const BASE_URL = "https://api.exchangerate.host";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export async function getCurrencies() {
